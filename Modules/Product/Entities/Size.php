53b3b4b9e1ac08aa190fbaff7b7e0ecce49b620a
dung.bk <dung.bk34@gmail.com>
2019-10-09 23:49:47 +0700

    sua lai seeder

--- Modules/Product/Entities/Size.php
+++ Modules/Product/Entities/Size.php
@@ -14,7 +14,7 @@ class Size extends Model
     ];
 
     public function products() {
-        return $this->belongsToMany(Product::class, 'product_sizes');
+        return $this->belongsToMany(Product::class, 'product_size');
     }
 
 }
--- database/seeds/ProductTableSeeder.php
+++ database/seeds/ProductTableSeeder.php
@@ -15,10 +15,10 @@ class ProductTableSeeder extends Seeder
         return $product = factory(\Modules\Product\Entities\Product::class, 1000)
         ->create()
         ->each(function($product) {
-            \Modules\Product\Entities\ProductSize::create([
+            DB::table('product_size')->insert([
                 'product_id' => $product->id,
                 'size_id' => rand(1,5),
-                'count' => rand(1,100),
+                'count' => rand(0,100),
             ]);
         });
     }
